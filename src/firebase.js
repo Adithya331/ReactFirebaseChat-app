5cfd4ae12b8a6ef26dbadd3800d53e8827f43586
Adithya331 <velaparthiadithya@gmail.com>
2023-08-23 17:35:27 +0530

    third commit

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,12 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDoYqADEQUzXcsEcv4AvDmw1wgf5AfgH8s",
-    authDomain: "chat-app-192e2.firebaseapp.com",
-    projectId: "chat-app-192e2",
-    storageBucket: "chat-app-192e2.appspot.com",
-    messagingSenderId: "848428619160",
-    appId: "1:848428619160:web:ccf91c7ecc417a55d587ed"
+  apiKey: "your-api-key",
+  authDomain: "your-authdomain",
+  projectId: "your-projectId",
+  storageBucket: "your-storageBucket",
+  messagingSenderId: "your-messagingSenderId",
+  appId: "your-appid"
 };
 
 export const app = initializeApp(firebaseConfig);
